45753c328518be1fbd821a2a905c437a6dc3a753
Filipe Madureira <filphil13@gmail.com>
2023-08-22 12:15:39 -0400

    new build

--- server.go
+++ server.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
 func GetHome(c *gin.Context) {
-	c.JSON(200,nil)
+	c.HTML(200, "index.html", nil)
 }
 
 // MAIN FUNCTION
 func main() {
 
 	router := gin.Default()
-	router.Use(static.Serve("/", static.LocalFile("./PersonalWebsite/dist", true)))
+	router.LoadHTMLGlob("./PersonalWebsite/dist/*.html")
 
 
 	//FRONT-END ENDPOINTS
