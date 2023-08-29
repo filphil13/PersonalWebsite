66fca647f4c54e6806721f648dab989768ca007d
Filipe Madureira <filphil13@gmail.com>
2023-08-28 21:06:43 -0400

    new new build

--- server.go
+++ server.go
@@ -5,12 +5,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func GetHome(c *gin.Context) {
+	c.JSON(200,nil)
+}
 // MAIN FUNCTION
 func main() {
 
 	router := gin.Default()
 	router.Use(static.Serve("/", static.LocalFile("./PersonalWebsite/dist", true)))
-
+	router.GET("/", GetHome)
 
 	router.Run()
 }
