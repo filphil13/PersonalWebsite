e38da2d00d3b4e04c4d4ba0df41e87e4f74ed45d
Filipe Madureira <filphil13@gmail.com>
2023-08-28 21:19:23 -0400

    still trying

--- server.go
+++ server.go
@@ -5,15 +5,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func GetHome(c *gin.Context) {
-	c.JSON(200, nil)
-}
 // MAIN FUNCTION
 func main() {
 
 	router := gin.Default()
 	router.Use(static.Serve("/", static.LocalFile("./Front-End/PersonalWebsite/dist", true)))
-	router.GET("/", GetHome)
 
 	router.Run()
 }
