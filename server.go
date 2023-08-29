01fab0d6a961550a7c7951442dc3f1c26df4d1da
Filipe Madureira <filphil13@gmail.com>
2023-08-28 21:28:02 -0400

    new attempt

--- server.go
+++ server.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"github.com/gin-gonic/contrib/static"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+func GetHome(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"content": "This is an index page...",
+	})
+}
 // MAIN FUNCTION
 func main() {
 
 	router := gin.Default()
-	router.Use(static.Serve("/", static.LocalFile("./Front-End/PersonalWebsite/dist", true)))
-
+	router.LoadHTMLGlob("./Front-End/PersonalWebsite/dist/*.html")
+	router.GET("/", GetHome)
 	router.Run()
 }
 
